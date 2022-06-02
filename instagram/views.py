bc224474c9be81d2c0912c759d6bfcf3f717c42a
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-02 21:54:36 +0300

    defined the signup view

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == POST:
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        pasword2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'That Email is already taken')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'That username is already taken')
+                return redirect('register')
+        else:
+            messages.info(request,'Passwords are not matching')
+            return redirect('register')
+    else:
+        user = User.objects.create_user(username=username, email=email,password=password)
+        user.save()
+    return render(request, 'register.html')
