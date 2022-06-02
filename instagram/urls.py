ec8ae3027e3db775c54b07c44ccfedef629330f5
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-02 22:27:50 +0300

    changes made on the login view and url path

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,7 +4,7 @@ from . import views
 #apps urls
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('register', views.register, name = 'register')
+    path('register', views.register, name = 'register'),
     path('login', views.login, name = 'login')
 ]
 
