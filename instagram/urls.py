d6fd2ec6a3ff2f33e9150363371b22c0197ff957
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-02 22:33:11 +0300

    created the logout url path

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('register', views.register, name = 'register'),
-    path('login', views.login, name = 'login')
+    path('login', views.login, name = 'login'),
+    path('logout', views.logout, name = 'logout'),
 ]
 
