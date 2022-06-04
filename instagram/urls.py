7dd3f1cc825e191220f6b64d2900ed4def59448c
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-05 01:38:54 +0300

    edited login path

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('register', views.register, name = 'register'),
-    path('login', views.login, name = 'login'),
+    path('login', views.login_request, name = 'login'),
     path('logout', views.logout, name = 'logout'),
+    path('settings', views.profile, name = 'profile'),
 ]
 
