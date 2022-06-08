84483d85bb47886d7e69f456bb85d12858925273
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-08 16:27:46 +0300

    updated the post app and the post model

---
+++ post/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from post.models import Post
+
+
+class NewPostform(forms.ModelForm):
+
+    picture = forms.ImageField(required=True)
+    caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags'}), required=True)
+
+    class Meta:
+        model = Post
+        fields = ['picture', 'caption', 'tags']
