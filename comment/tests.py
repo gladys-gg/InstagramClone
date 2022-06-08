e7acd6b50997b8957bc72536098be780e27261d8
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-06-08 17:39:10 +0300

    Adding the testing comment

--- comment/tests.py
+++ comment/tests.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 class CommentTestClass(TestCase):
     def setUp(self):
-        self.user = User.objects.create(id = 1, username='cherry')
+        self.user = User.objects.create(id = 4, username='maureen')
 
-        self.comment= Comment.objects.create(poster= self.user, comment='new comment' )
+        self.comment= Comment.objects.create(poster= self.user, comment='Pretty baby there' )
 
     def test_instance(self):
         self.assertTrue(isinstance(self.comment, Comment))
